4ae1a61fa420776ebf4c26f3b03b14d237188cc9
Antoine Riard <dev@ariard.me>
2023-09-03 19:32:52 +0100

    Add wip altnetd

--- src/bin/sync.rs
+++ src/bin/sync.rs
@@ -1,79 +1,79 @@
 //! The daemon to sync
 
-use altnet::control_capnp::control;
-use altnet::common::Capabilities;
+//use altnet::control_capnp::control;
+//use altnet::common::Capabilities;
+//
+//use capnp_rpc::{RpcSystem, twoparty, rpc_twoparty_capnp};
+//
+//use futures::{AsyncReadExt, FutureExt};
+//
+//use bitcoin::BlockHeader;
+//
+//
+//
+//struct SyncClient {
+//
+//
+//}
+//
+//struct SyncContext {
+//
+//	sync_clients: Vec<SyncClient>
+//
+//}
+//
+//struct SyncEngine {
+//
+//	best_header: BlockHeader,
+//}
+//
+//impl SyncEngine {
+//	fn new() {
+//
+//	}
+//
+//	fn request_headers() {
+//
+//	}
+//}
+//
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-use capnp_rpc::{RpcSystem, twoparty, rpc_twoparty_capnp};
+	//tokio::task::LocalSet::new().run_until(async move {
+	//	let socket_path = "127.0.0.1:10031";
 
-use futures::{AsyncReadExt, FutureExt};
+	//	let stream = tokio::net::TcpStream::connect(socket_path).await?;
+	//	stream.set_nodelay(true)?;
 
-use bitcoin::BlockHeader;
+	//	let (reader, writer) = tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
 
+	//	let network =
+	//		Box::new(twoparty::VatNetwork::new(reader, writer,
+	//					rpc_twoparty_capnp::Side::Client,
+	//					Default::default()));
 
+	//	let mut rpc_system = RpcSystem::new(network, None);
+	//	let control: control::Client = rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
+	//	tokio::task::spawn_local(Box::pin(rpc_system.map(|_| ())));
+	//	{
+	//		let request = control.print_hello_request();
+	//		let request_promise = request.send();
 
-struct SyncClient {
+	//		let response = request_promise.promise.await?;
 
+	//		loop {}
+	//	}
 
-}
+	//	// Step X : Boostrap the sync engine
+	//	let sync_engine = SyncEngine::new();
 
-struct SyncContext {
-
-	sync_clients: Vec<SyncClient>
-
-}
-
-struct SyncEngine {
-
-	best_header: BlockHeader,
-}
-
-impl SyncEngine {
-	fn new() {
-
-	}
-
-	fn request_headers() {
-
-	}
-}
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-	tokio::task::LocalSet::new().run_until(async move {
-		let socket_path = "127.0.0.1:10031";
-
-		let stream = tokio::net::TcpStream::connect(socket_path).await?;
-		stream.set_nodelay(true)?;
-
-		let (reader, writer) = tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
-
-		let network =
-			Box::new(twoparty::VatNetwork::new(reader, writer,
-						rpc_twoparty_capnp::Side::Client,
-						Default::default()));
-
-		let mut rpc_system = RpcSystem::new(network, None);
-		let control: control::Client = rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
-		tokio::task::spawn_local(Box::pin(rpc_system.map(|_| ())));
-		{
-			let request = control.print_hello_request();
-			let request_promise = request.send();
-
-			let response = request_promise.promise.await?;
-
-			loop {}
-		}
-
-		// Step X : Boostrap the sync engine
-		let sync_engine = SyncEngine::new();
-
-		loop {
-			sync_engine.process_headers();
-			sync_engine.relay_headers();
-		}
+	//	loop {
+	//		sync_engine.process_headers();
+	//		sync_engine.relay_headers();
+	//	}
 
 
-		Ok(())
-	}).await
+	//	Ok(())
+	//}).await
+	Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-pub mod control_capnp {
-	include!(concat!(env!("OUT_DIR"), "/control_capnp.rs"));
-}
-pub mod interfaces;
+//pub mod control_capnp {
+//	include!(concat!(env!("OUT_DIR"), "/control_capnp.rs"));
+//}
+//pub mod interfaces;
+
+pub mod nostr;
